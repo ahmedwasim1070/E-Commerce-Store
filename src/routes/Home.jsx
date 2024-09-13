181154f4391d0cbbed380e860ca58490ebe3fcf4
ahmedwasim1070@gmail.com <ahmedwasim1070@gmail.com>
2024-09-14 01:32:44 +0500

    setup

---
+++ src/routes/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../index.css'
+import Header from '../components/Header'
+
+function Home() {
+  return (
+    <div className='container mx-auto'>
+        <Header/>
+
+    </div>
+  )
+}
+
+export default Home
