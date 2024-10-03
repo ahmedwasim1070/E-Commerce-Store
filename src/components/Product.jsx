024ab093ef45fb31738510f8dd08a8c9e829e806
ahmedwasim1070 <ahmedwasim1070@gmail.com>
2024-10-03 07:55:09 -0700

    optimizing website

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,40 +8,25 @@ function Product() {
 
   const productList=productData.map((c,i)=>{
     
-    let [infoPop,setInfoPop]=useState(false);
-    
-    const handleResize=()=>{
-      window.innerWidth<=1024? setInfoPop(true): setInfoPop(false)
-    }
-
-    useEffect(()=>{
-      handleResize();
-      window.addEventListener('resize',handleResize);
-      return()=>{
-        window.removeEventListener('resize',handleResize);
-      };
-    },[]);
-
-    const hoverHandlers=window.innerWidth> 1024 ?{onMouseEnter:()=>setInfoPop(true),onMouseLeave:()=>setInfoPop(false)}:{};
+    let [bgChg,setBgChg]=useState(false);
 
     return(
-      <Link key={i} to={`/product/${i}`}>
-        <div  {...hoverHandlers} className='2xl:w-[350px] 2xl:h-[500px] xl:w-[350px] xl:h-[500px] lg:w-[350px] lg:h-[500px] md border border-[rgba(255,255,255,0.18)] rounded-2xl shadow-xl shadow-[rgba(255,255,255,0.1)] cursor-pointer transition-all duration-500 group hover:scale-[1.02] overflow-hidden relative'>
-          <div  className='w-full h-full 2xl:absolute xl:absolute lg:absolute md:relative md:flex md:justify-center'>
-            <img  src={`/productimg/${c.productImg}`}/>
-          </div>
-          {infoPop?
-            <div className='w-full h-full text-white 2xl:flex 2xl:flex-col 2xl:items-center 2xl:justify-center 2xl:bg-black 2xl:bg-opacity-30 2xl:backdrop-blur-md 2xl:gap-y-2 2xl:mt-0 xl:flex xl:flex-col xl:items-center xl:justify-center xl:bg-black xl:bg-opacity-30 xl:backdrop-blur-md xl:gap-y-2 xl:mt-0 lg:flex lg:flex-col lg:items-center lg:justify-center lg:bg-black lg:bg-opacity-30 lg:backdrop-blur-md lg:gap-y-2 lg:mt-0 md:flex md:flex-col md:items-start md:p-5 md:pt-5 md:bg-black md:bg-opacity-50 md:mt-4 sm:flex sm:flex-col sm:items-start sm:p-5 sm:pt-5 sm:bg-black sm:bg-opacity-50 sm:mt-4 esm:flex esm:flex-col esm:items-start esm:p-5 esm:pt-5 esm:bg-black esm:bg-opacity-50 esm:mt-4 '>
-              <p className='text-[1.5rem]'>{c.productName}</p>
-              <h2 className=' font-bold text-[1.5rem]'>Rs: {c.productPrice}</h2>
-              <p className='text-[15px] mb-2'>{c.productType}</p>
-              <button className='text-md outline-none  group-hover:underline  '>Buy Now &gt;&gt; </button>
-           </div>
-           :
-           ''
-          }
+      <section onMouseEnter={()=>setBgChg(true)} onMouseLeave={()=>setBgChg(false)}  className={`flex flex-col justify-center gap-y-5 hover:scale-110 hover:shadow-[rgba(0,0,0,0.6)] hover:shadow-2xl cursor-pointer  product-font`}>
+        <div className={`p-4 ${bgChg ? c.productBg : ''}`}>
+          <img loading='lazy' className='w-[320px] h-[420px]' src={`/productimg/${c.productImg}`} />
         </div>
-      </Link>
+        <div className='w-full pl-2'>
+          <h1 className='text-xl'>{c.productName}</h1>
+          <div className='flex items-center w-full my-1 gap-2'>
+            <span className='p-1.5 text-lg bg-red-500 text-white '>20% OFF</span>
+            <h1 className='text-xl text-red-600 font-bold'>PKR {c.productPrice/2}</h1>
+            <h1 className='text-xl line-through text-[rgba(0,0,0,0.4)] font-bold'>PKR {c.productPrice}</h1>
+          </div>
+          <div className='flex 2xl:justify-start xl:justify-start lg:justify-start md:justify-center sm:justify-center esm:justify-center pr-2'>
+            <button className='my-2 p-2.5 2xl:w-[40%] xl:w-[40%] lg:w-[40%] md:w-full sm:w-full esm:w-full  font-semibold border border-black transition-all duration-300 hover:bg-black hover:text-white'>Add to Cart</button>
+          </div>
+        </div>
+      </section>
     )
   });
 
@@ -49,12 +34,12 @@ function Product() {
 
   return (
     <>
-      <section className='w-full bg-black bg-opacity-30 bg backdrop-blur-md'>
+      <section className='w-full  py-10'>
         <div className='w-full h-20 mb-[100px]'>
-          <h1 className='text-white text-center text-[3rem] py-2'>Products</h1>
-          <hr className='w-[80%] mx-auto h-1 my-5'/>
+          <h1 className='text-center text-[3rem] py-2'>Products</h1>
+          <hr className='w-[85%] mx-auto h-1 my-5 bg-black'/>
         </div>
-        <div className='w-full h-100  flex  flex-wrap  justify-center px-[5%] gap-5  gap-y-10'>
+        <div className='w-full flex flex-wrap  justify-around px-[5%]  gap-y-10'>
           {productList}
         </div>
       </section>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -32,7 +32,7 @@ function Home() {
   }
 
   return (
-    <main className='bg-img w-full h-[100vh] overflow-x-hidden'>
+    <main className=' w-full h-[100vh] overflow-x-hidden'>
       <div className='w-full mx-auto fl:container'>
         {shCart?
         <Cart shCart={shCart} setShCart={setShCart} />
@@ -51,7 +51,7 @@ function Home() {
 
 
         {shSmNav&&
-          <div onClick={smNav} className='w-full h-[85vh] absolute top-[120px] flex justify-end bg-black bg-opacity-30 backdrop-blur-md overflow-y-hidden'>
+          <div onClick={smNav} className='w-full h-[85vh] absolute top-[120px] flex justify-end  overflow-y-hidden'>
             <div onClick={smNavClick} className='w-[50%] h-full bg-black bg-opacity-60 backdrop-blur-sm slide-in'>
               <ul className='w-full h-full text-white text-center text-[1rem] mt-8 flex flex-col  items-center'>
                 <li className='py-5 underline-animation-sm'>Home</li>
@@ -74,17 +74,17 @@ function Hero({idxPr,setIdxPr,addtoCart}){
   let [fade,setFade]=useState('fade-in');
 
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setFade('fade-out');
-      setTimeout(() => {
-        setIdxPr((prevIdx) => (prevIdx >= productData.length - 1 ? 0 : prevIdx + 1)); 
-        setFade('fade-in');
-      }, 300); 
-    }, 5000); 
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setFade('fade-out');
+  //     setTimeout(() => {
+  //       setIdxPr((prevIdx) => (prevIdx >= productData.length - 1 ? 0 : prevIdx + 1)); 
+  //       setFade('fade-in');
+  //     }, 300); 
+  //   }, 5000); 
 
-    return () => clearInterval(interval);
-  }, [idxPr]);
+  //   return () => clearInterval(interval);
+  // }, [idxPr]);
 
 
   const changeIdx = (direction) => {
@@ -100,18 +100,18 @@ function Hero({idxPr,setIdxPr,addtoCart}){
   };
 
   return(
-    <section className='w-full h-[85vh] bg-black bg-opacity-30 backdrop-blur-md  text-white flex justify-evenly md:relative sm:relative'>
-      <div className='h-[50%] flex flex-col justify-end 2xl:relative 2xl:left-0 2xl:top-0 2xl:w-[25%] xl:relative xl:left-0 xl:top-0 xl:w-[25%] lg:relative lg:top-0 lg:left-0 lg:w-[25%] md:absolute md:left-[52%] md:top-[-5%] md:w-full sm:absolute sm:left-[52%] sm:top-[-5%] sm:w-full esm:absolute esm:top-[25%] esm:left-[10%] '>
+    <section className={`w-full h-[85vh] flex justify-evenly md:relative sm:relative esm:relative ${fade} text-white ${productData[idxPr].productBg}`}>
+      <div className='h-[55%] flex flex-col justify-end 2xl:relative 2xl:left-0 2xl:top-0 2xl:w-[30%] xl:relative xl:left-0 xl:top-0 xl:w-[25%] lg:relative lg:top-0 lg:left-0 lg:w-[25%] md:absolute md:left-[52%] md:top-[-5%] md:w-full sm:absolute sm:left-[52%] sm:top-[-5%] sm:w-full esm:absolute esm:top-[25%] esm:left-[10%] '>
         <h1 className={`2xl:text-[4rem] xl:text-[3.2rem] lg:text-[2.5rem] md:text-[2.5rem] sm:text-[2.5rem] esm:text-[3rem] ${fade}`} >{productData[idxPr].productName}</h1>
         <p className={`2xl:text-[2rem] xl:text-[1.5rem] lg:text-[1.2rem] md:text-[1.5rem] sm:text-[1.2rem] esm:text-[1.5rem] mt-2 ${fade}`} >{productData[idxPr].productTagline}</p>
-        <p className={`2xl:text-[1rem] xl:text-[1rem] lg:text-[1rem] esm:mt-2  ${fade}`} >| {productData[idxPr].productType}</p>
+        <p className={`2xl:text-[1rem] xl:text-[1rem] lg:text-[1rem] esm:mt-2 `} >| {productData[idxPr].productType}</p>
       </div>
-      <div className='h-full flex flex-col justify-center 2xl:relative 2xl:left-0 2xl:top-0 2xl:w-[40%] xl:relative xl:left-0 xl:top-0 xl:w-[40%] lg:relative lg:left-0 lg:top-0 lg:w-[40%] md:absolute md:left-[12%] md:top-[0%] md:w-[40%] sm:absolute sm:left-[12%] sm:top-[0%] sm:w-[40%] esm:absolute esm:left-[10%] esm:top-[0%] esm:w-[250px]'>
-        <div className='mb-20'>
-          <img loading='lazy' className={`drop-shadow-${productData[idxPr].productColor}  2xl:w-[20vw] xl:w-[20vw] lg:w-[20vw] md:w-[30vw] sm:w-[30vw] esm:w-[250px] ${fade}`} src={`/productimg/${productData[idxPr].productImg}`} />
+      <div className='h-full flex flex-col justify-center ml-20 2xl:relative 2xl:left-0 2xl:top-0 2xl:w-[40%] xl:relative xl:left-0 xl:top-0 xl:w-[40%] lg:relative lg:left-0 lg:top-0 lg:w-[40%] md:absolute md:left-[12%] md:top-[0%] md:w-[40%] sm:absolute sm:left-[12%] sm:top-[0%] sm:w-[40%] esm:absolute esm:left-[10%] esm:top-[0%] esm:w-[250px]'>
+        <div className='mb-10'>
+          <img loading='lazy' className={`drop-shadow-${productData[idxPr].productColor}  2xl:w-[17vw] xl:w-[20vw] lg:w-[20vw] md:w-[30vw] sm:w-[30vw] esm:w-[250px] `} src={`/productimg/${productData[idxPr].productImg}`} />
         </div>
-        <div className='w-full flex mr-20'>
-          <button onClick={addtoCart} className='flex gap-2 border border-solid items-center mb-5  rounded-full bg-[#461111]  trnasition-all duration-300 outline-none  hover:bg-transparent 2xl:p-4 2xl:mt-10 xl:p-2 xl:mx-5 xl:mt-0 lg:p-2 lg:mt-0 lg:mx-2 md:p-3 md:mx-0 md:mt-5 sm:p-2 sm:mt-2 sm:mx-0 esm:mt-[220px] esm:p-2'>
+        <div className='w-full ml-10'>
+          <button onClick={addtoCart} className='flex gap-2 border border-solid items-center mb-5 rounded-full bg-transparent font-bold  trnasition-all duration-300 outline-none  hover:bg-black 2xl:p-4 2xl:mt-10 xl:p-2 xl:mx-5 xl:mt-0 lg:p-2 lg:mt-0 lg:mx-2 md:p-3 md:mx-0 md:mt-5 sm:p-2 sm:mt-2 sm:mx-0 esm:mt-[220px] esm:p-2'>
             <p className='text-xl text-center ml-2'>Add to Cart</p>
             <div className='flex items-center justify-center w-[24px] h-[24px]'>
               <h1 className='text-xl'>+</h1>
@@ -122,11 +122,11 @@ function Hero({idxPr,setIdxPr,addtoCart}){
       <div className='flex items-end 2xl:relative 2xl:left-0 2xl:top-0 2xl:w-[8%] 2xl:h-[55%] xl:relative xl:left-0 xl:top-0 xl:w-[8%] xl:h-[55%] lg:relative lg:top-0 lg:left-0 lg:w-[8%] lg:h-[55%] md:absolute md:left-[75%] md:top-[70%] md:w-full md:h-20 sm:absolute sm:top-[65%] sm:left-[75%] sm:h-20 sm:w-full esm:absolute esm:w-full esm:h-20 esm:top-[85%] esm:left-[75%]'>
         <div className='w-full'>
           <p className='2xl:text-[1.2rem] 2xl:my-0 xl:text-[1rem] xl:my-0 lg:text-[1rem] lg:my-1 md:text-[1rem] md:my-1 sm:text-[1rem] sm:my-1 esm:text-[1rem] esm:my-1'>Details :</p>
-          <h1 className={`2xl:text-2xl 2xl:my-2 xl:text-[1.4rem] xl:my-2 lg:text-[1.2rem] lg:my-1 md:text-[1.3rem] sm:text-[1.3rem] esm:text-[1.2rem]   ${fade}`}>Rs: {productData[idxPr].productPrice}</h1>
+          <h1 className={`2xl:text-2xl 2xl:my-2 xl:text-[1.4rem] xl:my-2 lg:text-[1.2rem] lg:my-1 md:text-[1.3rem] sm:text-[1.3rem] esm:text-[1.2rem]`}>Rs: {productData[idxPr].productPrice}</h1>
           <p className='2xl:text-[1rem] 2xl:my-0 xl:text-[1rem] xl:my-0 lg:text-[0.9rem] lg:my-0 md:text-[0.9rem] md:my-1 sm:text-[0.9rem] sm:my-1 esm:text-[0.8rem] esm:my-1 '>{productData[idxPr].productSize}</p>
           <div className=' flex items-center gap-3 2xl:mt-10 xl:mt-10 lg:mt-5 lg:gap-2 md:gap-2 md:mt-3 sm:gap-2 sm:mt-3 esm:mt-5 esm:gap-4'>
-            <button onClick={()=>changeIdx('prev')} className='border border-solid rounded-xl transition-all duration-300 hover:bg-[rgba(255,255,255,0.3)] 2xl:p-1 xl:p-1 lg:p-0.5 md:p-0.8 sm:p-0.8 esm:p-1'><svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 7L10 12L15 17" stroke="#f4f4f4" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/></svg></button>
-            <button onClick={()=>changeIdx('next')} className='border border-solid rounded-xl transition-all duration-300 hover:bg-[rgba(255,255,255,0.3)] 2xl:p-1 xl:p-1 lg:p-0.5 md:p-0.8 sm:p-0.8 esm:p-1'><svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 7L15 12L10 17" stroke="#f4f4f4" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/></svg></button>
+            <button onClick={()=>changeIdx('prev')} className='border border-solid rounded-xl transition-all duration-300 hover:bg-[rgba(0,0,0,0.3)] 2xl:p-1 xl:p-1 lg:p-0.5 md:p-0.8 sm:p-0.8 esm:p-1'><svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 7L10 12L15 17" stroke="#f3f3f3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/></svg></button>
+            <button onClick={()=>changeIdx('next')} className='border border-solid rounded-xl transition-all duration-300 hover:bg-[rgba(0,0,0,0.3)] 2xl:p-1 xl:p-1 lg:p-0.5 md:p-0.8 sm:p-0.8 esm:p-1'><svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 7L15 12L10 17" stroke="#f3f3f3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/></svg></button>
           </div>
         </div>
       </div>
